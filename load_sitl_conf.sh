ebef9cfa13a4ebf714fd828c6714ced1806f2ff2
Jonathan Cacace <jonathan.cacace@gmail.com>
2018-09-08 12:05:04 +0200

    new config file

---
+++ load_sitl_conf.sh
@@ -0,0 +1,7 @@
+source $(pwd)/../setup_gazebo.bash $(pwd)/../../ $(pwd)/../../build/posix_sitl_default
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/../../:$(pwd)
+
+export PX4_HOME_LAT=44.492813
+export PX4_HOME_LON=11.329982
+
+
